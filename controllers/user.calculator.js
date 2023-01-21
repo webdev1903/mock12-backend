053c77b1588143b7375c478a27a883bf9a1dd5b2
GeeksforGeeks <shreedharmore1109@gmail.com>
2023-01-21 15:29:31 +0530

    post

--- controllers/user.calculator.js
+++ controllers/user.calculator.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("", async (req, res) => {
+router.post("", async (req, res) => {
   try {
     let { amount, interest, time } = req.body;
     let i = interest / 100;
     // F = P [({(1+i) ^n}-1)/i]
-    const maturityValue = (amount * (((1 + i) ** time - 1) / i)).toFixed(2);
-    const investedAmount = (amount * time).toFixed(2);
-    const interestGained = (maturityValue - investedAmount).toFixed(2);
+    // console.log("x", amount, interest, time);
+    const maturityValue = amount * (((1 + i) ** time - 1) / i);
+    const investedAmount = amount * time;
+    const interestGained = maturityValue - investedAmount;
+    // console.log("y", maturityValue, interestGained, investedAmount);
     return res
       .status(200)
       .send({ maturityValue, investedAmount, interestGained });
